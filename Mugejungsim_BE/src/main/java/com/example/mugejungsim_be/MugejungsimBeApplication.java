591cf6f5ff280c7bbeea90263c09af02723803d0
abedcsj <abedcsj@gmail.com>
2024-12-21 16:36:14 +0900

    initial

---
+++ Mugejungsim_BE/src/main/java/com/example/mugejungsim_be/MugejungsimBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugejungsim_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugejungsimBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugejungsimBeApplication.class, args);
+	}
+
+}
---
+++ Mugejungsim_BE/src/test/java/com/example/mugejungsim_be/MugejungsimBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mugejungsim_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugejungsimBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
