4e0a6218eda3b725d4bef6982382153e0b05b812
abedcsj <abedcsj@gmail.com>
2024-12-30 18:47:06 +0900

    send image

--- Mugejungsim_BE/src/main/java/com/example/mugejungsim_be/CorsConfig.java
+++ Mugejungsim_BE/src/main/java/com/example/mugejungsim_be/CorsConfig.java
@@ -27,6 +27,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                 .allowedOrigins(
+                        "https://mugejunsim.store",
                         "http://localhost",         // 로컬 환경
                         "http://localhost:8080",    // 로컬 환경에서 명시적인 포트 추가
                         "http://172.17.208.113",    // iOS 디바이스에서 백엔드 접속 IP
